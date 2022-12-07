10034bd6bfda21f8f9faf59743e29b7de35c7042
Huu-Xiang <74952591+Huu-Xiang@users.noreply.github.com>
2022-12-06 21:10:47 -0500

    getting things ready for login system using Django

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.urls import path, include # new
+
 
 urlpatterns = [
     path('', views.index, name = 'index'),
--- myworld/myworld/urls.py
+++ myworld/myworld/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+#from django.urls import path, include # new
 
 urlpatterns = [
     path('members/', include('members.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    
 ]

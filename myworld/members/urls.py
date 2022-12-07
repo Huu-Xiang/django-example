7ef8187e98fcaccee9247a4cd5384e4d67d0f83b
Huu-Xiang <74952591+Huu-Xiang@users.noreply.github.com>
2022-12-06 22:15:48 -0500

    same as the last commit

--- myworld/members/urls.py
+++ myworld/members/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import path, include # new
 
+app_name = "main"
 
 urlpatterns = [
     path('', views.index, name = 'index'),
@@ -10,4 +11,8 @@ urlpatterns = [
     path('delete/<int:id>', views.delete, name='delete'),
     path('update/<int:id>', views.update, name='update'),
     path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+    # user ragistration path
+    path("", views.homepage, name="homepage"),
+    path("register", views.register_request, name="register")
+
 ]
--- myworld/members/views.py
+++ myworld/members/views.py
@@ -7,6 +7,12 @@ from django.urls import reverse
 from django.template import loader
 from .models import Members
 
+#for user registeration page
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 def index(request):
   mymembers = Members.objects.all().values()
   template = loader.get_template('index.html')
@@ -48,3 +54,16 @@ def updaterecord(request, id):
   member.lastname = last
   member.save()
   return HttpResponseRedirect(reverse('index'))
+
+def register_request(request):
+  if request.method == "POST":
+    form = NewUserForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      messages.success(request, "Registration successful." )
+      return redirect("main:homepage")
+      
+    messages.error(request, "Unsuccessful registration. Invalid information.")
+  form = NewUserForm()
+  return render (request=request, template_name="main/register.html", context={"register_form":form})

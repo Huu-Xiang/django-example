12e8469af6a3d4e6728ced18972efe0b2a1c59ca
Huu-Xiang <74952591+Huu-Xiang@users.noreply.github.com>
2022-12-08 16:37:26 -0500

    finish up the voting system

--- pollster_project/pollster/pollster/urls.py
+++ pollster_project/pollster/pollster/urls.py
@@ -28,4 +28,9 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name ='detail'),
     path('<int:question_id>/results/', views.results, name ='results'),
     path('<int:question_id>/vote/', views.vote, name ='vote'),
+    path('', views.index, name ='index'),
+
+    path('', include('pages.urls')),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
